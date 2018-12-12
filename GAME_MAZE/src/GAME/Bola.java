4400bae3e2d1feb30a28eccf0a4ac24b95b10765
SandraPc <SandraPc@Sandra>
2018-12-12 22:11:43 +0800

    versi 0.1

--- GAME_MAZE/src/GAME/Bola.java
+++ GAME_MAZE/src/GAME/Bola.java
@@ -5,17 +5,30 @@
  */
 package GAME;
 
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author SandraPc
  */
-class Bola extends Pixel{
+class Bola extends Pixel {
 
     public Bola(int x, int y) {
-        
+
         super(x, y);
-        
-   
-    
-}
+
+        URL lokasi = this.getClass().getResource("ball.jpg");
+        ImageIcon icon = new ImageIcon(lokasi);
+        Image image = icon.getImage();
+        this.setImage(image);
+
+    }
+    public void Gerak(int X, int Y){
+        int NX = this.getPosisix() + X ;
+        int NY = this.getPisisiy() + Y;
+        this.setPosisix(NX);
+        this.setPisisiy(NY);
+    }
 }
