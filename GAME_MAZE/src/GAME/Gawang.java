3823d93fe4fcfac7090aa412912556741c448647
SandraPc <SandraPc@192.168.56.1>
2018-12-13 23:49:32 +0800

    versi 0.1

--- GAME_MAZE/src/GAME/Gawang.java
+++ GAME_MAZE/src/GAME/Gawang.java
@@ -11,11 +11,16 @@ package GAME;
  */
 
 
+import java.awt.Image;
 import java.net.URL;
+import javax.swing.ImageIcon;
 
  class Gawang extends Pixel{
     public Gawang (int x, int y){
         super (x,y);
         
-        URL loc = this.getClass().getResource("gawang.jpg");
+        URL lokasi = this.getClass().getResource("gawang.jpg");
+        ImageIcon icon = new ImageIcon(lokasi);
+        Image image = icon.getImage();
+        this.setImage(image);
 }}
